72cb3a077ef4347d98b86e45793887c9f9eeedbf
Richard Walker <digitalsadhu@gmail.com>
2014-09-14 21:07:23 +0200

    initial commit

---
+++ run.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var fs   = require('fs')
+  , exec = require('exec')
+  , gulp = require('gulp')
+
+module.exports = function (jinn, cb) {
+  jinn.log('installing env')
+
+  function setup() {
+    var indexjs = process.cwd() + '/' + jinn.appname + '/index.js'
+    fs.readFile(indexjs, 'utf8', function (err, content) {
+      if (err) throw err
+      content = content.replace('\'use strict\';', '\'use strict\';\n\nrequire(\'envoodoo\')()')
+      fs.writeFile(indexjs, content, cb)
+    })
+  }
+
+  function install() {
+    var cd = 'cd ' + process.cwd() + '/' + jinn.appname
+    var cmd = 'npm install --save envoodoo'
+    exec(cd + ' && ' + cmd, setup)
+  }
+
+  function copy() {
+    var source = __dirname + '/templates/**/*'
+
+    gulp.src([source], {dot: true})
+      .pipe(gulp.dest(process.cwd() + '/' + jinn.appname))
+      .on('end', install)
+  }
+
+  copy()
+}
